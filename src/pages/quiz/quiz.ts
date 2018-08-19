d2e7f5b6c8f5bef13ead013e0507314a525ff3a3
sofia <sofiarivas1993@gmail.com>
2018-08-19 10:00:17 -0400

    Quiz

--- src/pages/quiz/quiz.ts
+++ src/pages/quiz/quiz.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+
 /**
  * Generated class for the QuizPage page.
  *
@@ -14,12 +15,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'quiz.html',
 })
 export class QuizPage {
+  mostrarP2:Boolean;
+  mostrarP1:Boolean;
+  mostrarP3:Boolean;
+  mostrarMensajeOk:Boolean;
+  mostrarP2:false;
+  mostrarP1=true;
+  mostrarP3=false;
+  mostrarMensajeOk:false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad QuizPage');
+  }
+  next():void{
+    this.mostrarP2=true;
+    this.mostrarP1=false;
+    this.mostrarP3=false;
+  }
+  next2():void{
+      this.mostrarP3=true;
+      this.mostrarP2=false;
+      this.mostrarP1=false;
+  }
+  finalquiz():void{
+      this.mostrarP3=false;
+      this.mostrarP2=false;
+      this.mostrarP1=false;
+      this.mostrarMensajeOk=true;
   }
 
 }
